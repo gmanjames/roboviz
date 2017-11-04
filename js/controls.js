2de2215d8b3a79cd98e7bfce6e99a0d7beae05fd
Brett Spatz <spatz519@gmail.com>
2017-11-03 22:44:56 -0500

    first commit wish me luck

---
+++ js/controls.js
@@ -0,0 +1,455 @@
+'use strict';
+
+/**
+ * controls.js:
+ */
+const Controls = (modelCtrls, playbackCtrls) =>
+{
+    /*
+     * The area onto which a log-file may be dragged and uploaded.
+     */
+    const dropZone = document.getElementById('dropZone');
+
+    /*
+     * Select element used to choose a particular animation if multiple are
+     * loaded.
+     */
+    const modelSelect = modelCtrls.querySelector('#modelName')
+
+    /*
+     * The name of a single group that is a component of the current model
+     * selected.
+     */
+    const groupSelect = modelCtrls.querySelector('#groupName');
+
+    /*
+     * Elements containing hexidecimal information for selecting a color for
+     * the current model group selected.
+     */
+    const colorControls = modelCtrls.querySelectorAll('.colors > a');
+
+    /*
+     * Range type input element for controlling the opacity of the current
+     * model group selected.
+     */
+    const transparency = modelCtrls.querySelector('.control-transparency input[type="range"]');
+
+    /*
+     *
+     */
+    const textureControls = modelCtrls.querySelectorAll('.textures > a');
+
+    /*
+     * Button for pausing and playing
+     */
+    const playPauseBtn = playbackCtrls.querySelector('#playPauseBtn');
+
+    /*
+     *
+     */
+    const playbackSpeed = playbackCtrls.querySelector('#modelSpeed');
+
+    /*
+     *
+     */
+    const playbackTime = playbackCtrls.querySelector('#modelTime');
+
+    /*
+     *
+     */
+    const playbackTimeVal = playbackCtrls.querySelector('#modelTimeVal');
+
+    /*
+     *
+     */
+    const rightTimeLabel = playbackCtrls.querySelector(".right-label[for='modelTime']");
+
+    /*
+     *
+     */
+    const leftTimeLabel = playbackCtrls.querySelector(".left-label[for='modelTime']");
+
+    /*
+     *
+     */
+    const playbackSdVal = playbackCtrls.querySelector('#modelSpeedVal');
+
+    /*
+     * List to contain visualizer objects
+     */
+    const visualizers      = {};
+
+    /*
+     *
+     */
+    let activeVisualizer;
+
+
+    /*
+     * init:
+     *
+     * ...
+     */
+    const init = function() {
+
+        // Check url for logfile referenece, or test model number.
+        const searchParams = new URLSearchParams(window.location.search);
+        if (searchParams.has('logref')) {
+            loadRefAnimation(searchParams.get('logref'));
+        }
+        else if (searchParams.has('test')) {
+            loadTestAnimation(parseInt(searchParams.get('test')));
+        }
+
+        addEventListeners();
+    };
+
+
+    /*
+     * notify:
+     *
+     * param time - the current time of the active animation.
+     *
+     * When the visualizer updates, the controls need to be notified of the
+     * updated time value in order to update control element's values.
+     */
+    const notify = function(time) {
+        time = time.toPrecision(3);
+        playbackTime.value = time;
+        playbackTimeVal.innerHTML = time;
+    };
+
+
+    /*
+     * updateControls:
+     *
+     * param modelInfo - The animation information for the currently active
+     * visualizer.
+     *
+     * ...
+     */
+    function updateControls(modelInfo) {
+
+        // Model controls
+        groupSelect.innerHTML = '';
+        for (let group of modelInfo.groups) {
+            let option = document.createElement('option');
+            option.value = group;
+            option.appendChild(document.createTextNode(group));
+            groupSelect.appendChild(option);
+        }
+
+        modelSelect.innerHTML = '';
+
+        let modelNumber = 0;
+        for (let vis in visualizers) {
+            modelNumber++;
+            let option = document.createElement('option');
+            option.value = 'model-' + modelNumber;
+            option.appendChild(document.createTextNode('model-' + modelNumber));
+            modelSelect.appendChild(option);
+        }
+
+        // Playback controls
+        playbackTime.min  = modelInfo.start;
+        playbackTime.max  = modelInfo.stop;
+        playbackTime.step = modelInfo.step;
+        rightTimeLabel.innerHTML = modelInfo.stop;
+        leftTimeLabel.innerHTML  = modelInfo.start;
+    }
+
+
+    /*
+     * addEventListeners:
+     *
+     * ...
+     */
+    function addEventListeners() {
+
+        // Drop zone event listeners
+        dropZone.addEventListener('drop', handleDrop, false);
+        dropZone.addEventListener('dragover', handleDragOver, false);
+
+        // Model controls
+        modelCtrls.querySelector('.toggle[for="model-controls"]').addEventListener('click', handleMenuToggle);
+        modelSelect.addEventListener('change', handleModelSelect);
+        groupSelect.addEventListener('change', handleGroupSelect);
+
+        for (let i = 0; i < colorControls.length; i++) {
+            colorControls[i].addEventListener('click', handleColor);
+        }
+
+        for (let i = 0; i < textureControls.length; i++) {
+            textureControls[i].addEventListener('click', handleTexture);
+        }
+
+        transparency.addEventListener('input', handleTransparency);
+
+        // Playback controls
+        playPauseBtn.addEventListener('click', handlePlayPause);
+        playbackSpeed.addEventListener('input', handleSpeed);
+        playbackTime.addEventListener('input', handleTime);
+    }
+
+
+    ////////////////////////////////////////////////////
+    //                Event Listeners                 //
+    ////////////////////////////////////////////////////
+
+    /*
+     * handleDrop:
+     *
+     * param evt - Javascript event
+     *
+     * Callback function for the Javascript 'drop' event used to handle a file
+     * being dropped within the area of the visualizer.
+     */
+    function handleDrop(evt) {
+
+        evt.stopPropagation();
+        evt.preventDefault();
+
+        let files = evt.dataTransfer.files; // FileList object.
+        loadDroppedAnimation(files[0]).then((evt) => {
+            return JSON.parse(evt.target.result);
+        }).then((dat) => {
+            if (activeVisualizer === undefined) {
+                activeVisualizer = Visualizer(60);
+                activeVisualizer.init();
+            }
+
+            visualizers[activeVisualizer] = activeVisualizer.loadAnimation(dat);
+            updateControls(visualizers[activeVisualizer]);
+        });
+    }
+
+
+    /*
+     * handleDragOver:
+     *
+     * param evt - Javascript event
+     *
+     * ...
+     */
+    function handleDragOver(evt) {
+        evt.preventDefault();
+        evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+    }
+
+
+    /*
+     * handleMenuToggle
+     *
+     * param evt - Javascript event
+     *
+     * ...
+     */
+    function handleMenuToggle(evt) {
+        const state = modelCtrls.dataset.state;
+        if (state === "collapsed") {
+            modelCtrls.dataset.state =  "expanded";
+        } else {
+            modelCtrls.dataset.state = "collapsed";
+        }
+    }
+
+
+    /*
+     * handleModelSelect
+     *
+     * param evt - Javascript evt
+     *
+     * ...
+     */
+    function handleModelSelect(evt) {
+        console.log(evt.target.value);
+    }
+
+
+    /*
+     * handleGroupSelect
+     *
+     * param evt - Javascript evt
+     *
+     * ...
+     */
+    function handleGroupSelect(evt) {
+        console.log(evt.target.value);
+    }
+
+
+    /*
+     * handleColor:
+     *
+     * param evt - Javascript evt
+     *
+     * ...
+     */
+    function handleColor(evt) {
+        let groupName = groupSelect.value;
+        activeVisualizer.changeColor(groupName, parseInt(evt.target.dataset.color));
+    }
+
+
+    /*
+     * handleTexture:
+     *
+     * param evt - Javascript evt
+     *
+     * ...
+     */
+    function handleTexture(evt) {
+        let groupName = groupSelect.value;
+        activeVisualizer.changeTexture(groupName, './assets/images/' + evt.target.dataset.texture + '.png');
+    }
+
+
+    /*
+     * handleTransparency
+     *
+     * param evt - Javascript event
+     *
+     * ...
+     */
+    function handleTransparency(evt) {
+        let groupName = groupSelect.value;
+        activeVisualizer.changeTransparency(groupName, parseFloat(evt.target.value));
+    }
+
+
+    /*
+     * handlePlayPause
+     *
+     * param evt - Javascript event
+     *
+     * Event handeler for pausing or playing the animation determined by the
+     * current state of the play/pause button.
+     */
+    function handlePlayPause(evt) {
+        const state = evt.target.dataset.toggle;
+        if (state === "play") {
+            activeVisualizer.pause();
+            evt.target.dataset.toggle = "pause";
+        } else {
+            activeVisualizer.play();
+            evt.target.dataset.toggle = "play";
+        }
+    }
+
+
+    /*
+     * handleTime
+     *
+     * param evt - Javascript event
+     *
+     * ...
+     */
+    function handleTime(evt) {
+        activeVisualizer.setTime(parseFloat(evt.target.value));
+        playbackTimeVal.innerHTML = evt.target.value;
+    }
+
+
+    /*
+     * handleSpeed
+     *
+     * param evt - Javascript event
+     *
+     * ...
+     */
+    function handleSpeed(evt) {
+        activeVisualizer.setSpeed(parseFloat(evt.target.value));
+        playbackSdVal.innerHTML = evt.target.value;
+    }
+
+
+    /*
+     * notifyOfTime:
+     *
+     * param timeVal - Current time value of the active visualizer.
+     *
+     * Accept time value from the visualizer neccessary to update controls.
+     */
+    function notifyOfTime(timeVal) {
+        playbackTimeVal.innerHTML = timeVal;
+    }
+
+
+    ////////////////////////////////////////////////////
+    //               Log-file Handeling               //
+    ////////////////////////////////////////////////////
+
+    /*
+     * loadDroppedAnimation:
+     *
+     * param file - dragged and dropped log-file
+     *
+     * Returns a promise that resolves to a model being loaded from file data
+     * converted to json.
+     */
+    function loadDroppedAnimation(file) {
+        let loaded;
+        return new Promise((resolve, reject) => {
+            let reader = new FileReader();
+            reader.readAsText(file, "UTF-8");
+
+            // On load resolve
+            reader.addEventListener('load', resolve);
+
+            // On progress, update progress bar
+            reader.addEventListener('progress', evt => {
+                console.log(evt.loaded / evt.total);
+            });
+        });
+    }
+
+
+    /*
+     * loadRefAnimation(urlRef):
+     *
+     * param urlRef - location of the logfile
+     *
+     * Returns a function that executes fetch of the GlobalFetch mixin from
+     * Fetch API. Method 'fetch' returns a promises that resolves to the
+     * successful aqcuisition of the resource, in this case, the json file.
+     */
+    function loadRefAnimation(urlRef) {
+        return () => {
+            fetch(urlRef).then((res) => res.json()).then((data) => {
+                if (activeVisualizer === undefined) {
+                    activeVisualizer = Visualizer(60);
+                    activeVisualizer.init();
+                }
+
+                visualizers[activeVisualizer] = activeVisualizer.loadAnimation(data);
+                updateControls(visualizers[activeVisualizer]);
+            });
+        }
+    }
+
+
+    /*
+     * loadTestAnimation:
+     *
+     * param animation - index for test model array.
+     *
+     * Returns a function that creates a model from the data held in the test
+     * model array at the specified index.
+     */
+    function loadTestAnimation(animation) {
+        if (activeVisualizer === undefined) {
+            activeVisualizer = Visualizer(60);
+            activeVisualizer.init();
+        }
+
+        visualizers[activeVisualizer] = activeVisualizer.loadAnimation(testModels[animation]);
+        updateControls(visualizers[activeVisualizer]);
+    }
+
+
+    // Constructed Controls object
+    return {
+        init,
+        notify
+    };
+};
---
+++ js/visualizer.js
@@ -0,0 +1,464 @@
+'use strict';
+
+/**
+ * app.js:
+ */
+const Visualizer = (fps) =>
+{
+    /*
+     * Three.js scene to render.
+     */
+    const scene = new THREE.Scene();
+
+    /*
+     * Simple perspective camera with aspect ratio, near and far clipping planes.
+     */
+    const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 4000);
+
+    /*
+     * The visualizer that also holds the camera element.
+     */
+    const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+
+    /*
+     * The area inside of which a log-file can be dropped to be uploaded.
+     */
+    const dropZone = document.getElementById('dropZone');
+
+    /*
+     * Texture Loader for loading new textures.
+     */
+    const textureLoader = new THREE.TextureLoader();
+
+    /*
+     * Desired time interval between frames.
+     */
+    const interval = 1000 / fps;
+
+    /*
+     * Clock used for tracking application time.
+     */
+    const clock = new THREE.Clock();
+
+    /*
+     * Pan, zoom, and orbit camera controls.
+     */
+    const controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+    /*
+     * Toggle between animation playing and paused states.
+     */
+    let isPlaying = true;
+
+    /*
+     * Multiplier for the speed and direction of the animation.
+     */
+    let playbackSpeed = 1;
+
+    /*
+     * Current time of the animation.
+     */
+    let currentTime = 0;
+
+    /*
+     * Global list to hold all currently loaded animations.
+     */
+    let animation;
+
+    /*
+     * Currently loaded texture.
+     */
+    let texture;
+
+
+    ////////////////////////////////////////////////////
+    //              Application Logic                 //
+    ////////////////////////////////////////////////////
+
+    /*
+     * init():
+     *
+     * Logic for setting up the initial Three.js scene, event listeners, etc.
+     */
+    const init = function() {
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        renderer.domElement.id = 'appCanvas';
+        document.body.appendChild( renderer.domElement );
+
+        camera.position.set(600, 600, 1000);
+        camera.lookAt(new THREE.Vector3(0,0,0));
+
+        // Directional light to enhance shadow.
+        let defaultLight = new THREE.DirectionalLight(0xffffff);
+        defaultLight.position.set(0, 1000, 0);
+        defaultLight.lookAt(new THREE.Vector3(0, 0, 0));
+        scene.add(defaultLight);
+
+        // Ambient light to make sure contrast isn't drastic.
+        let ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+        scene.add(ambientLight);
+		
+		// Scene background.
+		renderer.gammaInput = true;
+		renderer.gammaOutput = true;
+		renderer.setClearColor(0x001a0d, 1.0);
+		renderer.shadowMapEnabled = true;
+		
+		// Grid floor and fog to add perspective for model movement.
+		scene.fog = new THREE.Fog(0x001a0d, 1500, 4500);
+		var grid = new THREE.GridHelper(10000, 100, 0x001a0d, 0x006633);
+		scene.add(grid);
+	
+		
+		// Ground plane geometry matching grid size.
+        var groundGeometry = new THREE.PlaneGeometry(10000, 10000, 1, 1);
+		
+		// Transparent and not-shiny ground plane material.
+        var groundMaterial = new THREE.MeshLambertMaterial({
+			color: 0x4dffa6, 
+			transparent: true, 
+			opacity: 0.6, 
+			side: THREE.DoubleSide, 
+			emissive: 0x4dffa6,
+			// Helps solve z-plane clipping by off setting the ground plane from the grid.
+			polygonOffset: true,
+			polygonOffsetFactor: 1.0,
+			polygonOffsetUnits: 4.0
+		});
+		
+		// Create ground plane and rotate into horizontal position.
+        var ground = new THREE.Mesh(groundGeometry, groundMaterial);
+        ground.receiveShadow = true;
+        ground.rotation.x = -0.5 * Math.PI;
+  
+        // Add the ground plane to the scene.
+        scene.add(ground);
+		
+        // Enter animation loop.
+        animationLoop();
+
+        // Add event listener necessary for canvas resize.
+        window.addEventListener('resize', (evt) => {
+            const width  = evt.target.innerWidth,
+                  height = evt.target.innerHeight;
+            renderer.setSize(width, height);
+            camera.aspect = width / height;
+            camera.updateProjectionMatrix();
+        });
+    };
+
+
+    /*
+     * createModel:
+     *
+     * param data - JSON data for model parsed from file.
+     *
+     * Extract model information from JSON data.
+     */
+    function createModel(data)
+    {
+        let model  = new THREE.Group(),
+            frames = data.frames,
+            step   = data.step,
+            start  = data.start,
+            stop   = data.stop;
+
+            for (let group of data.groups) {
+                let comp = new THREE.Group(), // composite group to which we add objects
+                    geometry,
+                    material;
+
+                comp.name = group.name;
+
+                for (let obj of group.objs) {
+                    if (obj.type === "box") {
+                        geometry = new THREE.BoxBufferGeometry(obj.scale[0], obj.scale[1], obj.scale[2]);
+                        material = new THREE.MeshLambertMaterial( { color: parseInt(obj.color), overdraw: 0.5 } );
+                    }
+                    else if (obj.type === "cylinder") {
+                        geometry = new THREE.CylinderBufferGeometry(obj.scale[0], obj.scale[1], obj.scale[2], 32, 32);
+                        material = new THREE.MeshLambertMaterial( { color: parseInt(obj.color), overdraw: 0.5 } );
+                    }
+                    else if (obj.type === "ellipsoid") {
+                        geometry = new THREE.SphereBufferGeometry(obj.scale[0] * 0.5, 32, 32);
+                        material = new THREE.MeshLambertMaterial( { color: parseInt(obj.color), overdraw: 0.5 } );
+                        let matrix = new THREE.Matrix4();
+                        matrix.makeScale(1.0, obj.scale[1] / obj.scale[0], obj.scale[2] / obj.scale[0]);
+                        geometry.applyMatrix(matrix);
+                    }
+                    else if (obj.type === "sphere") {
+                        geometry = new THREE.SphereBufferGeometry(obj.diameter, 32, 32);
+                        material = new THREE.MeshLambertMaterial( { color: parseInt(obj.color),  overdraw: 0.5 } );
+                    }
+
+                    material.transparent = true;
+                    let mesh = new THREE.Mesh(geometry, material);
+                    comp.add(mesh);
+                }
+
+                model.add(comp);
+            }
+
+        scene.add(model);
+        animation = {model, step, start, stop, frames};
+    }
+
+
+    /*
+     * animationLoop:
+     *
+     * Game loop implementation for updating logical coordinates of models and
+     * rendering the scene.
+     */
+    function animationLoop() {
+        let then = Date.now();
+        let loop = () => {
+
+            requestAnimationFrame(loop);
+
+            let now   = Date.now(),
+                delta = now - then;
+
+            update();
+
+            if (delta >= interval) {
+                render();
+                then = Date.now();
+            }
+        }
+
+        loop();
+    }
+
+
+    /*
+     * initLoading:
+     *
+     * Called to begin loading animation. After the data is retrieved from the
+     * log-file the the loading animation initialized by this function should
+     * be stopped by the appropriate callback.
+     */
+    function initLoading() {
+        // here we need to clear scene
+        console.log('loading initialized');
+    }
+
+
+    /*
+     * update:
+     *
+     * Progress the time of the animation that will be used to calculate the
+     * current frame. Notify the controls of the change in time.
+     */
+    function update() {
+
+        let delta = clock.getDelta() * playbackSpeed;
+
+        if (isPlaying) {
+            currentTime += delta;
+        }
+
+        if (animation != null) {
+            updateModel();
+        }
+    }
+
+
+    /*
+     * updateModel:
+     *
+     * ...
+     */
+    function updateModel() {
+
+        if (currentTime < 0) {
+            currentTime = animation.stop;
+        }
+
+        let frame = Math.round((currentTime % animation.stop) / animation.step);
+
+        window.controls.notify(frame * animation.step);
+
+        for (const group of animation.model.children) {
+            group.position.set(animation.frames[frame][group.name].position[0],
+                animation.frames[frame][group.name].position[1],
+                animation.frames[frame][group.name].position[2]
+            );
+            group.quaternion.set(animation.frames[frame][group.name].quaternion[0],
+                animation.frames[frame][group.name].quaternion[1],
+                animation.frames[frame][group.name].quaternion[2],
+                animation.frames[frame][group.name].quaternion[3]
+            );
+        }
+    }
+
+
+    /*
+     * render:
+     *
+     * ...
+     */
+    function render() {
+        renderer.render( scene, camera );
+    }
+
+
+    ////////////////////////////////////////////////////
+    //              Visualizer Methods                //
+    ////////////////////////////////////////////////////
+
+    /*
+     * loadAnimation:
+     *
+     * param dat - Data for a new animation.
+     *
+     * ...
+     */
+    const loadAnimation = function(dat) {
+        createModel(dat);
+
+        // Return information about the animation loaded
+        let start = animation.start,
+            stop  = animation.stop,
+            step  = animation.step,
+            name  = animation.model.name,
+            groups = [];
+
+        for (const group of animation.model.children) {
+            groups.push(group.name);
+        }
+
+        return {
+            start,
+            stop,
+            step,
+            name,
+            groups
+        };
+    };
+
+
+    /*
+     * play:
+     *
+     * Begin or resume the animation
+     */
+    const play = function() {
+        isPlaying = true;
+    };
+
+
+    /*
+     * pause:
+     *
+     * Halt the animation at current position
+     */
+    const pause = function() {
+        isPlaying = false;
+    };
+
+
+    /*
+     * setTime:
+     *
+     * param timeVal - The position in the animation to play from
+     *
+     * Move the animation to the frame specified by the param timeVal
+     */
+    const setTime = function(timeVal) {
+        currentTime = timeVal;
+    };
+
+
+    /*
+     * setSpeed:
+     *
+     * param speedVal - Multiplier for playback speed
+     *
+     * Set the speed and direction of the animation
+     */
+    const setSpeed = function(speedVal) {
+        playbackSpeed = speedVal;
+    };
+
+
+    /*
+     * changeColor:
+     *
+     * param modelName - String of the model name to have it's color changed
+     * param color - String of the color to be changed in Hexadecimal
+     *
+     * Creates a new material in order to apply the new color
+     */
+    const changeColor = function(groupName, color) {
+        for (const group of animation.model.children) {
+            if (group.name == groupName) {
+                const oldTransparency = group.children[0].material.opacity;
+                let newMaterial = new THREE.MeshLambertMaterial( { color: color, overdraw: 0.5 } );
+                group.children[0].material = newMaterial;
+                group.children[0].material.transparent = true;
+                group.children[0].material.opacity = oldTransparency;
+            }
+        }
+    }
+
+
+    /*
+     * changeTexture:
+     *
+     * param modelName - String of the model name to have it's color changed
+     * param texture - String of the texture to be applied
+     *
+     * Creates a new material in order to apply the new texture using a path to the texture
+     */
+    const changeTexture = function(modelName, texturePath) {
+        texture = textureLoader.load(texturePath, function( newTexture ) {
+            let newMaterial = new THREE.MeshLambertMaterial( { map: newTexture, overdraw: 0.5 } );
+            for (const group of animation.model.children) {
+                if(group.name==modelName) {
+                    const oldTransparency = group.children[0].material.opacity;
+                    group.children[0].material = newMaterial;
+                    group.children[0].material.transparent = true;
+                    group.children[0].material.opacity = oldTransparency;
+                }
+            }
+        },
+        function (xhr) {
+          console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+        },
+        function (error) {
+          console.log( 'An error happened' );
+        });
+    }
+
+
+    /*
+     * changeTransparency:
+     *
+     * param modelName - String of the model name to have it's transparency changed
+     * param transparency - Floating point number between 0 and 1 that scales the opacity
+     *
+     * Change the transparency of the model
+     */
+    const changeTransparency = function(modelName, transparency) {
+        for (const group of animation.model.children) {
+            if(group.name==modelName) {
+                group.children[0].material.opacity = transparency;
+            }
+        }
+    }
+
+
+    // Constructed application object
+    return {
+        init,
+        loadAnimation,
+        play,
+        pause,
+        setSpeed,
+        setTime,
+        changeColor,
+        changeTexture,
+        changeTransparency
+    };
+};
