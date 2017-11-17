c9a0443b657900d0a8b66ae2d856dba9d7267e91
Ijames <yms1465@live.missouristate.edu>
2017-11-16 18:38:33 -0600

    Adding ability to reference hosted meshes

--- js/visualizer.js
+++ js/visualizer.js
@@ -31,6 +31,11 @@ const Visualizer = (fps) =>
     const textureLoader = new THREE.TextureLoader();
 
     /*
+     * STL loader for meshes in the STL format
+     */
+    const stlLoader = new THREE.STLLoader();
+
+    /*
      * Desired time interval between frames.
      */
     const interval = 1000 / fps;
@@ -152,7 +157,7 @@ const Visualizer = (fps) =>
      *
      * Extract model information from JSON data.
      */
-    function createModel(data)
+    async function createModel(data)
     {
         let model  = new THREE.Group(),
             frames = data.frames,
@@ -187,6 +192,10 @@ const Visualizer = (fps) =>
                         geometry = new THREE.SphereBufferGeometry(obj.diameter, 32, 32);
                         material = new THREE.MeshLambertMaterial( { color: parseInt(obj.color),  overdraw: 0.5 } );
                     }
+                    else if (obj.type === "mesh") {
+                        geometry = await loadData(obj.url);
+                        material = new THREE.MeshLambertMaterial( { color: parseInt(obj.color),  overdraw: 0.5 } );
+                    }
 
                     material.transparent = true;
                     let mesh = new THREE.Mesh(geometry, material);
@@ -198,6 +207,15 @@ const Visualizer = (fps) =>
 
         scene.add(model);
         animation = {model, step, start, stop, frames};
+    }
+
+
+    function loadData(url) {
+        return new Promise((resolve, reject) => {
+            stlLoader.load(url, geom => {
+                resolve(geom);
+            })
+        });
     }
 
 
